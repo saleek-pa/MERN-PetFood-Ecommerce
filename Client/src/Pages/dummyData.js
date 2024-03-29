d7e152d0daccd152c1408bef789a4fdba6fd4b3a
saleek-pa <saleek.pa@gmail.com>
2024-03-29 21:14:30 +0530

    Update .gitignore

---
+++ Client/src/Pages/dummyData.js
@@ -0,0 +1,51 @@
+export const categories = [
+  {
+    name: 'Cat Food',
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504912/home%20page/zcl6uneww32lxvmywlqr.jpg',
+    path: '/cat-food',
+  },
+  {
+    name: 'Cat Toys',
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504946/home%20page/zitnln1nvxqgik1crepq.jpg',
+  },
+  {
+    name: 'Dog Food',
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504966/home%20page/m9pfuvaf1ri1wxzvfknz.jpg',
+    path: '/dog-food',
+  },
+  {
+    name: 'Dog Toys',
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504990/home%20page/vrgxavjppnnjx3c7pphw.jpg',
+  },
+];
+
+export const services = [
+  {
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706505011/home%20page/hrokkodxtc3wnctbqmyz.png',
+    title: 'Free Same-Day Delivery',
+    description: 'Order by 2pm local time to get free delivery on orders $35+ today.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706505035/home%20page/kh3aywo5qniy8aew2lsa.png',
+    title: '30 Day Return',
+    description: '35% off your first order plus 5% off all future orders.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706505058/home%20page/midyf6ltwnn0ffv0queq.png',
+    title: 'Security payment',
+    description: '25% off your online order of $50+. Available at most locations.',
+  },
+  {
+    imageUrl: 'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706505081/home%20page/rlh6seaevj1nr1q0d3se.png',
+    title: '24/7 Support',
+    description: 'Shop online to get orders over $35 shipped fast and free.',
+  },
+];
+
+export const brandImages = [
+  'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504731/home%20page/kvep9kdldikmrq948ubu.jpg',
+  'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504787/home%20page/rwpmbmeynibqmrlixpdf.jpg',
+  'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504811/home%20page/tgwnpjfp8nfat96nw3xg.jpg',
+  'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504837/home%20page/fvtk5msccnfr6rktnzud.jpg',
+  'https://res.cloudinary.com/dmzqckfj4/image/upload/v1706504863/home%20page/izmg8yauavbqunlhs4hm.jpg',
+];
