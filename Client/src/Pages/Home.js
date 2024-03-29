994f8d92af9e9cbfc019dcd8fdfd3045bd575e3c
Mohamed Saleek PA <139626352+saleek-pa@users.noreply.github.com>
2024-03-29 21:06:25 +0530

    Update Home.js

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { brandImages, categories, services } from './dummyData';
 import { useNavigate } from 'react-router-dom';
 import { axios } from '../Utils/Axios';
 import ProductList from '../Components/ProductList';
